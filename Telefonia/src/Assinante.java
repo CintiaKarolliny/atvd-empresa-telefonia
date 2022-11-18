b5e5130f078123db66e14629e687adccd50ac20f
Cíntia Karolliny <79120035+CintiaKarolliny@users.noreply.github.com>
2022-11-18 00:16:32 -0400

    Refatoração na classe Assinante
    Mudança de "int" para "Integer", mudanças no constructor e implementação do restante dos getters.

--- Telefonia/src/Assinante.java
+++ Telefonia/src/Assinante.java
@@ -1,32 +1,78 @@
-import java.util.ArrayList;
-
-//!!!!!!!!ATRIBUTO CHAMADA ESTA INCORRETO!!!!!!!!!!!!!
+import java.util.Arrays;
 
 public class Assinante {
-	private long cpf;
-	private String nome;
-	private int numero; 
-//	private int chamadas;
-	protected int NumChamdas;
+	protected long cpf;
+	protected String nome;
+	protected Integer numero;
+
+	protected Chamada[] chamadas;
+
+	protected Integer numChamadas;
 	
-	public Assinante(long cpf, String nome, int numero) {
+	public Assinante(long cpf, String nome, Integer numero, Integer numChamadas) {
 		this.cpf = cpf;
 		this.nome = nome;
-		this.numero = numero; 
+		this.numero = numero;
+
+		this.numChamadas = numero;
+		this.chamadas = new Chamada[numero];
+
+		Chamada[] chamadas = new Chamada[numChamadas];
+
+/*		for (int i = 0; i < chamadas.length; i++) {
+			chamadas[i] = new Chamada();
+			System.out.println(chamadas[i]);
+		} */
 	}
 	
 	public long getCpf() {
 		return cpf;
 	}
 
-    @Override
+	public String getNome() {
+		return nome;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public Chamada[] getChamadas() {
+		return chamadas;
+	}
+
+	public Integer getNumChamadas() {
+		return numChamadas;
+	}
+
+	public void setCpf(long cpf) {
+		this.cpf = cpf;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public void setChamadas(Chamada[] chamadas) {
+		this.chamadas = chamadas;
+	}
+
+	public void setNumChamadas(Integer numChamadas) {
+		this.numChamadas = numChamadas;
+	}
+
+	@Override
 	public String toString() {
-    	return cpf + nome + numero; 
+    	return cpf + nome + numero + numChamadas + Arrays.toString(chamadas);
 	}
 
-	public static void main(String[] args) {
-		Assinante teste = new Assinante();
+	/* public static void main(String[] args) {
+		Assinante teste = new Assinante(481545058,"Cíntia", 2);
 
-	}
+	}*/
 
 }
