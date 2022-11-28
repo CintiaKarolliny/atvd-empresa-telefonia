d31b760e42385632b85835d04b08d7000aab3ee8
Eduardo0409 <du.caldas.alves@gmail.com>
2022-11-28 00:07:50 -0300

    Subindo Telefonia com implementacao do menu e alguns metodos

--- Telefonia/src/Telefonia.java
+++ Telefonia/src/Telefonia.java
@@ -1,94 +1,146 @@
+package Projeto;
+
 import java.util.GregorianCalendar;
 import java.util.*;
 
 public class Telefonia {
+	private PrePago[] prePagos = new PrePago[5];
+	int numPrePagos = 0;
+	
+	private PosPago[] posPagos = new PosPago[5];
+	int numPosPagos = 0;
+	
+	//Método cadastrarAssinante
+	
+    public void cadastrarAssinante( ) {
+    	long cpf;
+    	String nome;
+    	int numero;
+    	
+        System.out.println("+==================================+");
+        System.out.println("|01 - Pré-pago                     |");
+        System.out.println("|02 - Pós-pago                     |");
+        System.out.println("+==================================+");
+        
+        System.out.println("Olá, por favor escolha a assinatura que desejar!");
+        Scanner assinatura = new Scanner(System.in);
+        int tipoAssinatura = assinatura.nextInt();
 
-    public static void main(String[] args) {
+        switch(tipoAssinatura){
+            case 01:
+            	if(numPrePagos >= prePagos.length) {
+                	System.out.println("Atenção! Número de assinantes atingido.");
+                	break;
+            	}
+            	
+                System.out.println("Cadastro de pré-pago selecionado ");
+                System.out.println("Entre com cpf do assinante: ");
+                cpf = assinatura.nextLong();
 
-        // Começo Estruturação Menu
+                System.out.println("Entre com nome do assinante: ");
+                nome = assinatura.next();
 
-        Scanner entrada = new Scanner(System.in);
+                System.out.println("Entre com número do assinante: ");
+                numero = assinatura.nextInt();
 
-        int opcao = entrada.nextInt();
+                PrePago novoAssinantePre = new PrePago(cpf, nome, numero, 0, 0);
+                
+                
+                prePagos[numPrePagos] = novoAssinantePre;
+                numPrePagos += 1;
+                System.out.println("Novo Assinante cadastrado: ");
+                System.out.println(novoAssinantePre.toString());
+                break;
+                
+            case 02:
+            	if(numPosPagos >= posPagos.length) {
+                	System.out.println("Atenção! Número de assinantes atingido.");
+                	System.exit(0);
+                }
+            	
+                System.out.println("Cadastro de pós-pago selecionado ");
+                System.out.println("Entre com cpf do assinante: ");
+                cpf = assinatura.nextLong();
 
-        System.out.println(opcao);
+                System.out.println("Entre com nome do assinante: ");
+                nome = assinatura.next();
 
-        while (opcao > 0) {
+                System.out.println("Entre com número do assinante: ");
+                numero = assinatura.nextInt();
 
-            switch (opcao) {
-                case 1:
-                    System.out.println("\nNUMERO 1 AQ");
-                    break;
-                case 2:
-                    System.out.println("\nNUMERO 2 AQ");
-                    break;
-                case 3:
-                    System.out.println("\nNUMERO 3 AQ");
-                    break;
-                default:
-                    System.out.println("\nERRO AQ");
-                    break;
-
-            }
+                PosPago novoAssinantePos = new PosPago(cpf, nome, numero, 0, 0);
+                posPagos[numPosPagos] = novoAssinantePos;
+                numPosPagos += 1;
+                System.out.println("Novo Assinante cadastrado: ");
+                System.out.println(novoAssinantePos.toString());
+                break;
+                
+            default:
+                System.out.println("Opção inválida.");
+                break;
         }
-        System.out.println("Opção Inválida! Tente Novamente");
-
-        Scanner entrada2 = new Scanner(System.in);
-        int opcao = entrada2.nextInt();
-
-        // ******** Testes cadastro de assinante PréPago
-
-        System.out.println("\nCadastro de Assinantes: ");
-
-        System.out.println("\nInsira o CPF: ");
-        Scanner entradaCPF = new Scanner(System.in);
-
-        long cpf = entradaCPF.nextLong();
-
-        System.out.println("\nInsira o nome: ");
-        Scanner entradaNome = new Scanner(System.in);
-
-        String nome = entradaNome.next();
-
-        System.out.println("\nInsira o número: ");
-        Scanner entradaNumero = new Scanner(System.in);
-
-        Integer numero = entradaNumero.nextInt();
-
-        PrePago pre1 = new PrePago(cpf, nome, numero);
-
-        System.out.println(pre1.getCpf());
-
-        System.out.println(pre1.getNome());
-
-        System.out.println(pre1.getNumero());
-
-        // ******** Teste fazerChamada PréPago
-
-        System.out.print("\nEntre com a data (ddmmaaaa): ");
-        Scanner entradaData1 = new Scanner(System.in);
-        Integer data = entradaData1.nextInt();
-
-        System.out.print("\nEntre com a duração em minutos: ");
-        Scanner entradaDuracao1 = new Scanner(System.in);
-        Integer duracao = entradaData1.nextInt();
-
-        // ******** Testes Chamada
-
-        GregorianCalendar ca = new GregorianCalendar();
-        ca.set(2018, GregorianCalendar.DECEMBER, 12);
-
-        GregorianCalendar ca2 = new GregorianCalendar();
-        ca2.set(2020, GregorianCalendar.APRIL, 3);
-
-        Chamada chamada1 = new Chamada(ca.getTime(), 50);
-        Chamada chamada2 = new Chamada(ca2.getTime(), 30);
-
-        Set<Chamada> chamadas = new HashSet<>();
-
-        chamadas.add(chamada1);
-        chamadas.add(chamada2);
-
-        System.out.print(chamadas);
+    }
+	
+    public void listarAssinantes( ) {
+    	System.out.println("Listando assinantes");
+    	System.out.println("Assinantes pré-pagos: ");
+    	for(int i = 0; i < prePagos.length; i++) {
+    		if(prePagos[i] == null) {
+    			continue;
+    		}
+    		System.out.println(prePagos[i].toString());
+    	}
+    	System.out.println("Assinantes pós-pagos: ");
+    	for(int i = 0; i < posPagos.length; i++) {
+    		if(posPagos[i] == null) {
+    			continue;
+    		}
+    		System.out.println(posPagos[i].toString());
+    	}
+    }
+    
+    public void fazerChamada ( ) {
+    	long cpf;
+    	
+    	   System.out.println("+==================================+");
+           System.out.println("|01 - Pré-pago                     |");
+           System.out.println("|02 - Pós-pago                     |");
+           System.out.println("+==================================+");
+           
+           System.out.println("Olá, por favor selecione a sua assinatura!");
+           
+    	Scanner indicar = new Scanner(System.in);
+    	int tipoAssinatura = indicar.nextInt();
+    	
+    	switch(tipoAssinatura){
+        case 1:
+        	System.out.println("Cadastro de pré-pago selecionado ");
+        	System.out.println("Por favor, insira seu CPF");
+        	cpf = indicar.nextLong();
+        break;
+        	
+        case 2:
+        	System.out.println("Cadastro de pós-pago selecionado ");
+        	System.out.println("Por favor, insira seu CPF");
+        	cpf = indicar.nextLong();
+        	
+        break;
+    	}	
+    }
+    
+    public static void main(String[] args) {
+        //Menu
+        System.out.println("Por favor, selecione uma opção abaixo.");
+        System.out.println("+==================================+");
+        System.out.println("|               MENU               |");
+        System.out.println("+==================================+");
+        System.out.println("|01 - Cadastrar Assinante          |");
+        System.out.println("|02 - Listar Assinantes            |");
+        System.out.println("|03 - Fazer chamada                |");
+        System.out.println("|04 - Fazer recarga                |");
+        System.out.println("|05 - Imprimir faturas             |");
+        System.out.println("|06 - Sair do programa             |");
+        System.out.println("+==================================+");
+        
     }
 }
