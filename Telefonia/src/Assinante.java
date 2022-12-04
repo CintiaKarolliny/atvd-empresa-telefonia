3af604973e51b8c664fd989ee66f5c29d651a110
Gustavo Tunes <gustavosouzatunes@gmail.com>
2022-12-04 17:05:40 -0300

    Modificação da classe Assinante

--- Telefonia/src/Assinante.java
+++ Telefonia/src/Assinante.java
@@ -1,78 +1,28 @@
-import java.util.Arrays;
-
 public abstract class Assinante {
-	protected long cpf;
-	protected String nome;
-	protected Integer numero;
-
+	private long cpf;
+	private String nome;
+	private long numero;
 	protected Chamada[] chamadas;
-
 	protected Integer numChamadas;
-	
-	public Assinante(long cpf, String nome, Integer numero, Integer numChamadas) {
+
+	public Assinante(long cpf, String nome, long numero, Integer numChamadas) {
+		super();
 		this.cpf = cpf;
 		this.nome = nome;
 		this.numero = numero;
 
-		this.numChamadas = numero;
-		this.chamadas = new Chamada[numero];
+		this.chamadas = new Chamada[numChamadas];
+		this.numChamadas = numChamadas;
 
 		Chamada[] chamadas = new Chamada[numChamadas];
-
-/*		for (int i = 0; i < chamadas.length; i++) {
-			chamadas[i] = new Chamada();
-			System.out.println(chamadas[i]);
-		} */
 	}
 	
 	public long getCpf() {
-		return cpf;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public Integer getNumero() {
-		return numero;
-	}
-
-	public Chamada[] getChamadas() {
-		return chamadas;
-	}
-
-	public Integer getNumChamadas() {
-		return numChamadas;
-	}
-
-	public void setCpf(long cpf) {
-		this.cpf = cpf;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public void setNumero(Integer numero) {
-		this.numero = numero;
-	}
-
-	public void setChamadas(Chamada[] chamadas) {
-		this.chamadas = chamadas;
-	}
-
-	public void setNumChamadas(Integer numChamadas) {
-		this.numChamadas = numChamadas;
+		return this.cpf;
 	}
 
 	@Override
 	public String toString() {
-    	return "CPF: " cpf + "/nNome: " + nome + "/nNúmero: " + numero + "/nNúmero de chamadas: " + numChamadas + Arrays.toString(chamadas);
+    	return "CPF: " + cpf + " | Nome: " + nome + " | Telefone: " + numero;
 	}
-
-	/* public static void main(String[] args) {
-		Assinante teste = new Assinante(481545058,"Cíntia", 2);
-
-	}*/
-
 }
