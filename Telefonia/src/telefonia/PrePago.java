b62958a72d02b565ad5acd00627146ba2e740206
Eduardo0409 <du.caldas.alves@gmail.com>
2022-12-05 19:37:58 -0300

    Revert "Inserção da classe no package, alterações no mostrarChamadas e implementação do imprimirFatura"
    This reverts commit 8de325559e098348fc1c78ec58b8db444d7f8880.

--- Telefonia/src/telefonia/PrePago.java
+++ Telefonia/src/telefonia/PrePago.java
@@ -1,23 +1,20 @@
-package telefonia;
-
-import java.util.Calendar;
 import java.util.Date;
 import java.lang.Math;
-import java.util.GregorianCalendar;
 
 public class PrePago extends Assinante {
-    protected float creditos;
+    private float creditos;
     private int numRecargas;
 
     private Recarga[] recargas;
 
 
-    public PrePago(long cpf, String nome, long numero, int numRecargas, float creditos) {
+    public PrePago(long cpf, String nome, int numero, int numRecargas, float creditos) {
         super(cpf, nome, numero, numRecargas);
         this.creditos = creditos;
-
         this.recargas = new Recarga[numRecargas];
         this.numRecargas = numRecargas;
+
+        Recarga[] recargas = new Recarga[numRecargas];
     }
 
     public void recarregar(Date data, float valor) {
@@ -44,7 +41,7 @@ public class PrePago extends Assinante {
 
                 System.out.println("\nChamada realizada!\nQuantidade de chamadas restantes: " + this.numChamadas + "\nQuantidade de créditos restantes: R$" + Math.round(this.creditos * 100.0) / 100.0);
             } else {
-                System.out.println("\nCréditos insuficientes!\nVocê possui R$" + creditos + ", enquanto esta chamada usaria R$" + duracao*1.45f);
+                System.out.println("\nCréditos insuficientes!");
             }
         } else {
             System.out.println("\nNão é possível fazer mais chamadas!");
@@ -53,94 +50,32 @@ public class PrePago extends Assinante {
 
     public void mostrarChamadas() {
         int i = 0;
+        int cont = chamadas.length;
 
         while (i < chamadas.length) {
-            if (chamadas[i] == null) {
-                i++;
-            } else {
-                System.out.println("\n" + this.chamadas[i].toString());
-                i++;
-            }
+            System.out.println("\n" + cont + "ª chamada: " + this.chamadas[i]);
+            i++;
+            cont--;
         }
     }
 
     public void mostrarRecargas() {
         int i = 0;
+        int cont = recargas.length;
 
         while (i < recargas.length) {
-            if (recargas[i] == null) {
-                i++;
-            } else {
-                System.out.println("\n" + this.recargas[i].toString());
-                i++;
-            }
+            System.out.println("\n" + cont + "ª recarga: " + this.recargas[i]);
+            i++;
+            cont--;
         }
     }
 
-    public void imprimirFatura(int mes) {
+	/* public void imprimirFatura(int mes) {
 
-        GregorianCalendar dataCal = new GregorianCalendar();
+		if (mes == getmesDataFormatada() + 1) {
+			System.out.println("CPF: " + cpf + "\nNome: " + nome + "\nNúmero de Telefone: " + numero + "Chamadas: " + chamadas + "Crédito restante: " + creditos + "Quantidade de recargas feitas: " + numRecargas);
+		}
+		System.out.println("Não há faturas correspondentes a este mês!");
 
-        int i = 0;
-        int cont = 0;
-
-        float totalChamada = 0;
-        float totalRecarga = 0;
-
-        while (i < this.chamadas.length) {
-
-            if (this.chamadas[i] != null) {
-                dataCal.setTime(this.chamadas[i].getData());
-
-                int mesCal = dataCal.get(Calendar.MONTH);
-
-                if (mes == mesCal + 1) {
-
-                    int duracao = this.chamadas[i].getDuracao();
-
-                    totalChamada += ((duracao * 1.45f)*100.0)/100.0;
-
-                    System.out.println(this.chamadas[i].toString() + "\nValor da chamada: R$" + Math.round((duracao * 1.45f)*100.0)/100.0);
-
-                    i++;
-                } else {
-                    i++;
-                }
-
-            } else {
-                System.out.println("\nNão há chamadas para este mês!");
-                i++;
-            }
-        }
-
-        System.out.println("\nFatura total das chamadas: R$" + totalChamada);
-
-        System.out.println("\n");
-
-        while (cont < this.recargas.length) {
-
-        if (this.recargas[cont] == null) {
-            cont++;
-        } else {
-                int mesCal = dataCal.get(Calendar.MONTH);
-
-                if (mes == mesCal + 1) {
-
-                    totalRecarga += this.recargas[cont].getValor();
-
-                    System.out.println(this.recargas[cont].toString());
-                    cont++;
-                } else {
-                    System.out.println("\nNão há recargas para este mês");
-                    cont++;
-                }
-
-            }
-
-        }
-
-        System.out.println("\nValor total das recargas: R$" + Math.round((totalRecarga*100.0)/100.0) + "\nTotal de Créditos: R$" + Math.round(this.creditos*100.0)/100.0);
-
-    }
-
+	} */
 }
